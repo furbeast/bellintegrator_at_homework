de4b53cb54f6c9de31963ea68d06a2fd1fd3f9f4
furbeast <slonn91@mail.ru>
2020-08-05 21:29:43 +0300

    create result list map

--- src/test/java/ru/yandex/market/PageObjectYandexMarketWithSearch.java
+++ src/test/java/ru/yandex/market/PageObjectYandexMarketWithSearch.java
@@ -1,6 +1,8 @@
 package ru.yandex.market;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,14 +10,31 @@ import java.util.Map;
 
 public class PageObjectYandexMarketWithSearch {
 
-    private String selectorSearchItems = "//article[@class=\"_1O1OnAPlSR _29bSn5MwO8 cia-vs cia-cs\"]";
+    private String selectorResultItems = "//article[@class=\"_1O1OnAPlSR _29bSn5MwO8 cia-vs cia-cs\"]";
+    private String selectorResultVendor = "//article[@class=\"_1O1OnAPlSR _29bSn5MwO8 cia-vs cia-cs\"]//div[@class=\"_1NyIdwOZ6-\"]";
+    private String selectorResultModel = "//article[@class=\"_1O1OnAPlSR _29bSn5MwO8 cia-vs cia-cs\"]//h3//span";
+    private String selectorResultPrice = "//article[@class=\"_1O1OnAPlSR _29bSn5MwO8 cia-vs cia-cs\"]//div[@class=\"_1PaCzxbbzN _3f2ZtYT7NH _2Sc2mRSDwb\"]//span/span[1]";
 
     private WebDriver driver;
 
-    private List<Map<String, Object>> collectResults = new ArrayList<>();
+    private List<WebElement> searchItsems = new ArrayList<>();
+    private List<Map<String,Object>> collectResults = new ArrayList<>();
 
     public PageObjectYandexMarketWithSearch(WebDriver driver) {
         this.driver = driver;
         driver.get("https://market.yandex.ru/catalog--smartfony/16814639/list?glfilter=7893318%3A153043");
+        searchItsems = driver.findElements(By.xpath(selectorResultItems));
+    }
+
+    public List<Map<String, Object>> getCollectResults() {
+        for (WebElement result : searchItsems) {
+            collectResults.add(Map.of(
+                    "WEB_ELEMENT", result,
+                    "VENDOR", result.findElement(By.xpath(selectorResultVendor)).getText(),
+                    "Model", result.findElement(By.xpath(selectorResultModel)).getText(),
+                    "PRICE", result.findElement(By.xpath(selectorResultPrice)).getText()
+            ));
+        }
+        return collectResults;
     }
 }
--- src/test/java/ru/yandex/market/Tests.java
+++ src/test/java/ru/yandex/market/Tests.java
@@ -2,10 +2,17 @@ package ru.yandex.market;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.Map;
+
 public class Tests extends WebDriverSettings{
 
     @Test
     public void testTask2() {
         PageObjectYandexMarketWithSearch iphone = new PageObjectYandexMarketWithSearch(chromeDriver);
+
+        List<Map<String, Object>> resultSearch = iphone.getCollectResults();
+
+        resultSearch.stream().forEach(x -> System.out.println(x.get("VENDOR").toString()));
     }
 }
