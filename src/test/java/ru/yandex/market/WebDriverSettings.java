425a775d6ee52b463693da28e62142413227a036
furbeast <slonn91@mail.ru>
2020-08-17 23:48:35 +0300

    add Docker WebDriverSettings not debug

--- src/test/java/ru/yandex/market/WebDriverSettings.java
+++ src/test/java/ru/yandex/market/WebDriverSettings.java
@@ -4,11 +4,15 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 public class WebDriverSettings {
 
+    // Local Machine
     WebDriver chromeDriver;
 
     @BeforeEach
@@ -25,4 +29,26 @@ public class WebDriverSettings {
     public void closeTest() {
         chromeDriver.quit();
     }
+
+
+
+    // Docker
+    /*RemoteWebDriver chromeDriver;
+
+    @BeforeEach
+    public void setupBrowser() throws Exception{
+        final DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+        capabilities.setVersion("84.0");
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", false);
+        chromeDriver = new RemoteWebDriver(new URL("http://127.0.0.1:4444/wd/hub"), capabilities);
+    }
+
+    @AfterEach
+    public void closeTest() {
+        if(chromeDriver!=null) {
+            chromeDriver.quit();
+            chromeDriver = null;
+        }
+    }*/
 }
